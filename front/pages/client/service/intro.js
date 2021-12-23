db84d5117497b2398addae47bada6e7ceee67597
Kim-Jin-Uk <gottkfthf@gmail.com>
2021-12-23 18:30:32 +0900

    CREATE - client service intro page

--- front/pages/client/service/intro.js
+++ front/pages/client/service/intro.js
@@ -1,12 +1,146 @@
 import React from 'react'
 import Header from "../../../components/Header";
 import styles from '../../../styles/Intro.module.scss'
-import Link from 'next/link';
+import { Steps } from 'antd';
+import { createGlobalStyle } from 'styled-components';
+
+const { Step } = Steps;
+
+const Global = createGlobalStyle`
+  .ant-steps{
+    min-width: 269px;
+    display: inline-block;
+    vertical-align: top;
+    width: auto;
+    margin-top: 60px;
+    margin-left: 7px!important;
+  }
+  
+  .ant-steps-item-tail{
+    display: none !important;
+  }
+  
+  .ant-steps-item-container{
+    height: 115px;
+  }
+  
+  .ant-steps-item-content{
+    padding-top: 8px;
+    padding-left: 12px;
+  }
+  
+  .ant-steps-item-title{
+    font-family: Spoqa Han Sans Neo;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 14px !important;
+    line-height: 150% !important;
+    color: #FFFFFF !important;
+  }
+  
+  .ant-steps-item-description{
+    margin-top: 4px;
+    font-family: Spoqa Han Sans Neo;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 14px !important;
+    line-height: 150%;
+    color: #C1D9FC !important;
+  }
+  
+  @media (max-width: 600px) {
+    .ant-steps-item-description{
+      width: 269px !important;
+    }
+  }
+
+  @media (min-width: 600px) and (max-width: 1024px) {
+    .ant-steps{
+      margin-top: 80px;
+      margin-left: 12px!important;
+    }
+    
+    .ant-steps-item-description{
+      width: 400px !important;
+    }
+
+    .ant-steps-item-title{
+      font-size: 16px !important;
+    }
+
+    .ant-steps-item-description{
+      margin-top: 8px;
+      font-size: 16px !important;
+    }
+
+    .ant-steps-item-container{
+      height: 127px;
+    }
+
+    .ant-steps-item-content{
+      padding-top: 6px;
+      padding-left: 48px;
+    }
+  }
+
+  @media (min-width: 1024px) {
+    .ant-steps{
+      margin-top: 80px;
+      margin-left: 12px!important;
+    }
+
+    .ant-steps-item-description{
+      width: 400px !important;
+    }
+
+    .ant-steps-item-title{
+      font-size: 16px !important;
+    }
+
+    .ant-steps-item-description{
+      margin-top: 8px;
+      font-size: 16px !important;
+    }
+
+    .ant-steps-item-container{
+      height: 127px;
+    }
+
+    .ant-steps-item-content{
+      padding-top: 6px;
+      padding-left: 48px;
+    }
+  }
+`;
 
 const Intro = () => {
 
+    const customDot = (dot, { status, index }) => (
+        index === 0
+        ?
+            <div className={styles.dots_group}>
+                <div className={styles.big_dots}>
+                    <div className={styles.small_dots_top}></div>
+                </div>
+                <div className={styles.first_line}></div>
+            </div>
+        :
+            index === 6
+            ?
+                <div>
+                    <div className={styles.small_dots}></div>
+                    <div className={styles.finish_line}></div>
+                </div>
+            :
+                <div>
+                    <div className={styles.small_dots}></div>
+                    <div className={styles.after_line}></div>
+                </div>
+    );
+
     return(
         <>
+            <Global/>
             <Header></Header>
             <div className={styles.background_top}>
                 <div className={styles.content_cover_top}>
@@ -67,16 +201,155 @@ const Intro = () => {
                         효율적으로
                     </div>
                 </div>
-                <div>content2</div>
-                <div>content3</div>
+
+                <div className={`${styles.contents_sub_middle} ${styles.content_sub_1}`}>
+                    <div className={styles.contents_sub_text}>
+                        메이커와 추가 계약을 맺으면
+                        <br/>
+                        수수료 혜택을 드립니다.
+                    </div>
+                    <div className={`${styles.contents_sub_img} ${styles.content_img_1}`}></div>
+                </div>
+                <div className={`${styles.contents_sub_middle} ${styles.content_sub_2}`}>
+                    <div className={`${styles.contents_sub_text} ${styles.content_text_sub_2}`}>
+                        메이커와 소통문제로 제작에 차질이 생기면 계약금 100% 환불을 책임집니다.
+                    </div>
+                    <div className={`${styles.contents_sub_img} ${styles.content_img_2}`}></div>
+                </div>
             </div>
             {/*manuals*/}
-            <div>
-                <div>manual1</div>
-                <div>manual2</div>
-                <div>manual3</div>
-                <div>manual4</div>
-                <div>manual5</div>
+            <div className={styles.manual_title}>
+                제작 현장에서
+                <br/>
+                필요한 매뉴얼을 받아보세요
+            </div>
+            <div className={styles.manual_grouper} >
+                <div className={styles.manual_group}>
+                    <div className={styles.manu_back1}>
+                        <div className={styles.manual_text}>
+                            집행 계약서
+                            <br/>
+                            양식
+                        </div>
+                        <div className={`${styles.manual_img} ${styles.manu1}`}></div>
+                    </div>
+                    <div className={styles.manu_back2}>
+                        <div className={styles.manual_text}>
+                            콘셉트 기획서
+                            <br/>
+                            메뉴얼
+                        </div>
+                        <div className={`${styles.manual_img} ${styles.manu2}`}></div>
+                    </div>
+                    <div className={styles.manu_back3}>
+                        <div className={styles.manual_text}>
+                            현장 스케줄 관리
+                            <br/>
+                            메뉴얼
+                        </div>
+                        <div className={`${styles.manual_img} ${styles.manu3}`}></div>
+                    </div>
+                    <div className={styles.manu_back4}>
+                        <div className={styles.manual_text}>
+                            현장 스토리보드
+                            <br/>
+                            예시
+                        </div>
+                        <div className={`${styles.manual_img} ${styles.manu4}`}></div>
+                    </div>
+                    <div className={styles.manu_back5}>
+                        <div className={styles.manual_text}>
+                            예산 견적서
+                            <br/>
+                            양식
+                        </div>
+                        <div className={`${styles.manual_img} ${styles.manu5}`}></div>
+                    </div>
+                </div>
+            </div>
+            <div className={styles.steps_group_middle}>
+                <div className={`${styles.manual_title} ${styles.steps_title}`}>
+                    믿을 수 있는 메이커를
+                    <br/>
+                    선별해서 매칭합니다
+                </div>
+                <div className={styles.steps_group}>
+                    <div className={styles.steps_number}>
+                        <div>1</div>
+                        <div>2</div>
+                        <div>3</div>
+                        <div>4</div>
+                        <div>5</div>
+                        <div>6</div>
+                        <div>7</div>
+                    </div>
+
+                    <Steps
+                        progressDot={customDot} current={6} direction="vertical" size={"small"}
+                        style={{marginLeft:"39px"}}
+                    >
+                        <Step
+                            title={"요구조건 작성"}
+                            description={'제공드리는 작성 매뉴얼에 따라 콘텐츠에 필요한 메이커에 대한 요구조건을 작성해주세요.'}
+                        />
+                        <Step
+                            title={"검토"}
+                            description={'전달해주신 요구조건을 검토해 파트너스 메이커에서 조건에 맞는 메이커를 선별합니다. 검토는 1~3일이 소요됩니다.'}
+                        />
+                        <Step
+                            title={"메이커 매칭"}
+                            description={'선별한 메이커와 요구조건의 세부사항을 논의하고 작업 견적을 확정하는 과정을 진행합니다.'}
+                        />
+                        <Step
+                            title={"계약"}
+                            description={'메이커 매칭에 대한 서비스 보장의 내용이 담긴 계약서를 작성합니다. 메이커 참여에 대해 100% 보장합니다.'}
+                        />
+                        <Step
+                            title={"콘텐츠 집행"}
+                            description={'메이커와의 협업과 제작중인 콘텐츠가 원할하게 집행되도록 전문가들의 노하우 메뉴얼을 제공합니다.'}
+                        />
+                        <Step
+                            title={"정산"}
+                            description={'계약 조건에 따라 계약금을 정산합니다. 메이커와 연속 계약을 진행할 수 있도록 도와드립니다.'}
+                        />
+                        <Step
+                            title={"후기"}
+                            description={'메이커와의 작업후기를 남길 수 있으며, 메이커와 연속 계약을 진행할 수 있도록 도와드립니다.'}
+                        />
+                    </Steps>
+                </div>
+            </div>
+
+            <div className={styles.background_bottom}>
+                <div>
+                    <div className={styles.question_title_bottom}>자주 묻는 질문</div>
+                    <div style={{marginTop:"32px"}}>
+                        <div className={styles.question_card}>
+                            <div className={styles.question_icon}></div>
+                            <div className={styles.question_content}>계약금은 어떻게 되나요?</div>
+                        </div>
+                        <div className={styles.answer_group}>
+                            <div className={styles.answer_icon}></div>
+                            <div className={styles.answer_content}>
+                                계약금은 용역비 90%로와 중개비 10%로 나뉩니다. 예를 들어 계약금이 100만원이면 90%인 90만원은 메이커에게 지불되는 용역비로, 10%인 10만원은 brmn에게 중개비로 지불됩니다.
+                            </div>
+                        </div>
+                    </div>
+                    <div style={{marginTop:"32px"}}>
+                        <div className={styles.question_card}>
+                            <div className={styles.question_icon}></div>
+                            <div className={styles.question_content}>메이커의 역량은 어떻게 확인하나요?</div>
+                        </div>
+                        <div className={styles.answer_group}>
+                            <div className={styles.answer_icon}></div>
+                            <div className={styles.answer_content}>
+                                brmn 파트너스 메이커의 이력과 포트폴리오로 확인할 수 있습니다.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className={styles.match_btn}></div>
             </div>
         </>
     )
