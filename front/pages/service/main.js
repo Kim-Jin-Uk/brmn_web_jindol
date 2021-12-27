dda58b03fc1c909b74825a10ba55974e0ee98935
Kim-Jin-Uk <gottkfthf@gmail.com>
2021-12-27 13:45:20 +0900

    UPDATE - (full page) intro Page

--- front/pages/service/main.js
+++ front/pages/service/main.js
@@ -8,13 +8,36 @@ const Main = () => {
     return(
         <>
             <Header></Header>
+            {/*main title*/}
             <div className={styles.main_title}>지금 우리와 함께 하세요</div>
+            {/*intro top btn*/}
             <div className={styles.main_contents_wrapper}>
-                <div></div>
-                <div></div>
-                <div></div>
+                <div>
+                    <Link href={"/"}><a>
+                        <div>title</div>
+                        <div>img</div>
+                        <div>text</div>
+                    </a></Link>
+                </div>
+
+                <div>
+                    <Link href={"/"}><a>
+                        <div>title</div>
+                        <div>img</div>
+                        <div>text</div>
+                    </a></Link>
+                </div>
+
+                <div>
+                    <Link href={"/"}><a>
+                        <div>title</div>
+                        <div>img</div>
+                        <div>text</div>
+                    </a></Link>
+                </div>
             </div>
-            <div>
+            {/*intro content*/}
+            <div className={styles.main_intro_content}>
                 다양한 콘텐츠가
                 만들어지는
                 세상. 당신의
@@ -22,8 +45,16 @@ const Main = () => {
                 함께할 사람들을
                 찾아보세요.
             </div>
-
+            {/*intro bottom btn*/}
             <Link href={"/"}><a><div></div></a></Link>
+            {/*bottom area*/}
+            <div></div>
+
+            <iframe
+                width="100%" height="500px" src="https://www.youtube.com/embed/sxmCmaHJ0kc"
+                frameBorder={"none"}
+                >
+            </iframe>
         </>
     )
 }
